d67b2e2bd794b17bbe745d21ff4f42ce72d2f549
Magnesium- <yyong007@outlook.com>
2023-03-26 22:35:43 +0800

    feat: components and docs

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # icontainer
+
+一个容器组件，默认使用 `div`， 然后使用
+
+## 组件列表
+
+|组件名|说明|
+|---|---|
+|`<ContainerCenter />`|容器组件（居中布局）|
+
+```css
+.iContainerCenter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+```
+
+使用 flex 布局居中处理
+
+### 使用与 pc 的容器组件
+
+|组件名|说明|
+|---|---|
+|`<ContainerCenter.XS />`|断点长度是：XS(480)|
+|`<ContainerCenter.SM />`|断点长度是：MD(480)|
+|`<ContainerCenter.MD />`|断点长度是：SM(768)|
+|`<ContainerCenter.LG />`|断点长度是：SM(992)|
+|`<ContainerCenter.XL />`|断点长度是：SM(1200)|
+|`<ContainerCenter.XXL />`|断点长度是：SM(1600)|
+
+### 使用与 iPhone 的容器组件
+
+|组件名|说明|
+|---|---|
+|`<ContainerCenter.iPhone4 />`|iPhone 4|
+|`<ContainerCenter.iPhone5 />`|iPhone 5|
+|`<ContainerCenter.iPhone5S />`|iPhone 5S|
+|`<ContainerCenter.iPhone5C />`|iPhone 5C|
+|`<ContainerCenter.iPhoneSE />`|iPhone SE|
+|`<ContainerCenter.iPhone6 />`|iPhone 6|
+
+## 说明
+
+api 展现形式还在探索之中，可能随着需求发生变化。
--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -12,3 +12,117 @@ export const ContainerCenter = (props: ContainerCenterProps) => {
     </div>
   );
 };
+
+ContainerCenter.XS = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-XS"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter.SM = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-SM"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter.MD = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-MD"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter.LG = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-LG"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter.XL = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-XL"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter.XXL = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-XXL"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter["iPhone4"] = ContainerCenter["iPhone4S"] = (
+  props: ContainerCenterProps
+) => {
+  return (
+    <div
+      className="iContainerCenter-iPhone-4-4S"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter["iPhone5"] =
+ContainerCenter["iPhone5S"] = 
+ContainerCenter["iPhone5C"] = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-iPhone-5-5S-5C"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter["iPhoneSE"] = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-iPhone-SE"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
+
+ContainerCenter["iPhone6"] = (props: ContainerCenterProps) => {
+  return (
+    <div
+      className="iContainerCenter-iPhone-6"
+      style={props.styles ? props.styles : {}}
+    >
+      {props.children}
+    </div>
+  );
+};
